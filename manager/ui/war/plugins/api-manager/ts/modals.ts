1b2f56aa4631e7f96b9af181d476d724233556c8
Florian Volk <florian.volk@scheer-group.com>
2018-06-11 12:14:18 +0200

    GW-75: Load all entries per default

--- manager/ui/war/plugins/api-manager/ts/modals.ts
+++ manager/ui/war/plugins/api-manager/ts/modals.ts
@@ -127,6 +127,8 @@ module ApimanModals {
                             'value': '%' + $scope.searchText + '%',
                             'operator': 'like'
                         });
+                        body.page = 1;
+                        body.pageSize = 0x7fffffff; // Java Interger.MAX_VALUE
 
                         var searchStr = angular.toJson(body);
 
