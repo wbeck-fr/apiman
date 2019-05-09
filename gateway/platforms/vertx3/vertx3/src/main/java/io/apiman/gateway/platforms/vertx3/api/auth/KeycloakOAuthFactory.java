18ef79f1c252a6a9a9e51bf80363e466f1886e0b
Florian Volk <florian.volk@scheer-group.com>
2019-05-09 16:07:41 +0200

    GW-184: Upgrade vert.x from 3.4.2 to 3.7.0 to get removeEnabledSecureTransportProtocol()

--- gateway/platforms/vertx3/vertx3/src/main/java/io/apiman/gateway/platforms/vertx3/api/auth/KeycloakOAuthFactory.java
+++ gateway/platforms/vertx3/vertx3/src/main/java/io/apiman/gateway/platforms/vertx3/api/auth/KeycloakOAuthFactory.java
@@ -19,12 +19,15 @@ package io.apiman.gateway.platforms.vertx3.api.auth;
 import io.apiman.common.util.Basic;
 import io.apiman.gateway.platforms.vertx3.common.config.VertxEngineConfig;
 import io.apiman.gateway.platforms.vertx3.verticles.ApiVerticle;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpClientOptions;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.core.net.JksOptions;
+import io.vertx.ext.auth.User;
 import io.vertx.ext.auth.oauth2.AccessToken;
 import io.vertx.ext.auth.oauth2.OAuth2Auth;
 import io.vertx.ext.auth.oauth2.OAuth2FlowType;
@@ -98,10 +101,10 @@ public class KeycloakOAuthFactory {
                 HttpClientOptions sslOptions = getHttpClientOptionsForKeycloak(authConfig);
 
                 OAuth2Auth oauth2 = KeycloakAuth.create(vertx, flowType, authConfig, sslOptions);
-                oauth2.getToken(params, tokenResult -> {
+                oauth2.authenticate(params, tokenResult -> {
                     if (tokenResult.succeeded()) {
                         log.debug("OAuth2 Keycloak exchange succeeded.");
-                        AccessToken token = tokenResult.result();
+                        AccessToken token = (AccessToken) tokenResult.result();
                         token.isAuthorised(role, res -> {
                             if (res.result()) {
                                 context.next();
@@ -145,6 +148,12 @@ public class KeycloakOAuthFactory {
             public AuthHandler addAuthorities(Set<String> authorities) {
                 return this;
             }
+
+            @Override
+            public void parseCredentials(RoutingContext routingContext, Handler<AsyncResult<JsonObject>> handler) {}
+
+            @Override
+            public void authorize(User user, Handler<AsyncResult<Void>> handler) {}
         };
     }
 
