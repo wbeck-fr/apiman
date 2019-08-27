fc13760d6f50df90ca9bd3d87fc04594d1b1fd52
Florian Volk <florian.volk@scheer-group.com>
2019-08-27 13:46:23 +0200

    GW-210: Change code style because of apiman PR: Fix null pointer exception in caching resources policy

--- gateway/engine/policies/src/main/java/io/apiman/gateway/engine/policies/CachingResourcesPolicy.java
+++ gateway/engine/policies/src/main/java/io/apiman/gateway/engine/policies/CachingResourcesPolicy.java
@@ -167,8 +167,10 @@ public class CachingResourcesPolicy extends AbstractMappedDataPolicy<CachingReso
     @Override
     protected IReadWriteStream<ApiResponse> responseDataHandler(final ApiResponse response,
                                                                 IPolicyContext context, CachingResourcesConfig policyConfiguration) {
-        // if the response is empty because of a policy failure before we end here and return null
-        if (response == null) return null;
+        if (response == null) {
+            // if the response is empty because of a policy failure before we end here and return null
+            return null;
+        }
 
         List<CachingResourcesSettingsEntry> possibleMatchingCachingEntries = context.getAttribute(CACHE_POSSIBLE_MATCHING_ENTRIES, new ArrayList<CachingResourcesSettingsEntry>());
         boolean isAMatch = false;
