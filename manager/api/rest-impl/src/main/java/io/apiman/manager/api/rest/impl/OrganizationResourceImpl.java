2787b39e8934fc871053577648039cc98241d3be
Florian Volk <florian.volk@scheer-group.com>
2020-01-08 13:33:34 +0100

    GW-212: Fix security issue with api-key

--- manager/api/rest-impl/src/main/java/io/apiman/manager/api/rest/impl/OrganizationResourceImpl.java
+++ manager/api/rest-impl/src/main/java/io/apiman/manager/api/rest/impl/OrganizationResourceImpl.java
@@ -1014,6 +1014,8 @@ public class OrganizationResourceImpl implements IOrganizationResource {
     @Override
     public ContractBean getContract(String organizationId, String clientId, String version,
             Long contractId) throws ClientNotFoundException, ContractNotFoundException, NotAuthorizedException {
+        if (!securityContext.hasPermission(PermissionType.clientView, organizationId))
+            throw ExceptionFactory.notAuthorizedException();
         try {
             storage.beginTx();
             ContractBean contract = storage.getContract(contractId);
